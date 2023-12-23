42c83465947ff66e53eab92d975fcad789f770b5
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2023-12-23 20:28:45 +0800

    通过 shadcn/ui 完成侧边栏和仪表盘

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// 合并 className
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
