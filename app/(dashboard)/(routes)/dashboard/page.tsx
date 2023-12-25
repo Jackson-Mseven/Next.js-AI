306ce273a0909657997b688e067cf86296817e91
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2023-12-25 20:48:32 +0800

    修改dashboard列表顺序

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -15,13 +15,6 @@ const tools = [
     href: '/conversation'
   },
   {
-    label: 'Music Generation',
-    icon: Music,
-    color: 'text-emerald-500',
-    bgColor: 'bg-emerald-500/10',
-    href: '/music'
-  },
-  {
     label: 'Image Generation',
     icon: ImageIcon,
     color: 'text-pink-700',
@@ -34,6 +27,13 @@ const tools = [
     color: 'text-orange-700',
     bgColor: 'bg-orange-700/10',
     href: '/video'
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+    href: '/music'
   },
   {
     label: 'Code Generation',
