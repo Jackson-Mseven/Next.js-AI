d5dee53b2a44abaeddaad02a6e08ac3308e786f8
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2024-01-15 22:51:28 +0800

    首页 UI 优化

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,16 +3,18 @@ import Link from 'next/link'
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page
-      <div>
-        <Link href="/sign-in">
-          <Button>
+    <div className="flex justify-center items-center  w-screen h-screen bg-[url('../public/bg.jpg')]">
+      <div className='flex items-center flex-col bg-white/20 w-1/3 h-2/3 rounded-3xl'>
+        <div className='text-pink-950 h-24 flex items-center'>
+          Choose Your Way To Landing My Dashboard.
+        </div>
+        <Link href="/sign-in" className='w-2/3 h-1/3'>
+          <Button className='w-full h-full bg-transparent hover:bg-transparent'>
             Login
           </Button>
         </Link>
-        <Link href="/sign-up">
-          <Button>
+        <Link href="/sign-up" className='w-2/3 h-1/3'>
+          <Button className='w-full h-full bg-transparent hover:bg-transparent'>
             Register
           </Button>
         </Link>
